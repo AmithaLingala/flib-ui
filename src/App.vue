571a7761aba3f774f36871bcb2f976b79bd25cfc
Coding Otaku <otaku@codingotaku.com>
2020-09-11 21:11:15 +0530

    update footer

--- src/App.vue
+++ src/App.vue
@@ -39,8 +39,8 @@
           <a href="https://codingotaku.com">Coding Otaku</a>.
           The source code is licensed
           <a
-            href="http://opensource.org/licenses/mit-license.php"
-          >MIT</a>. The website content
+            href="https://github.com/codingotaku/flib-ui/blob/master/LICENSE"
+          >GNU GENERAL PUBLIC LICENSE Version 2</a>. The website content
           is licensed
           <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
         </p>
